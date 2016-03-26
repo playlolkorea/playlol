a67a2e6aa0d0aa9cd972a7120c4e96ebfd2eb989
Nechrito <nechrito@live.se>
2016-03-26 23:38:32 +0100

    Twitch

---
+++ Nechrito Twitch/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+
+namespace Nechrito_Twitch
+{
+    internal class Program
+    {
+        private static readonly Obj_AI_Hero Player = ObjectManager.Player;
+        public static Menu Menu;
+        private static Orbwalking.Orbwalker _orbwalker;
+
+        private static void Main() => CustomEvents.Game.OnGameLoad += OnGameLoad;
+
+        private static void OnGameLoad(EventArgs args)
+        {
+            if (Player.ChampionName != "Twitch") return;
+            Game.PrintChat(
+                "<b><font color=\"#FFFFFF\">[</font></b><b><font color=\"#00e5e5\">Nechrito Twitch</font></b><b><font color=\"#FFFFFF\">]</font></b><b><font color=\"#FFFFFF\"> Version: 1</font></b>");
+            Game.PrintChat(
+                "<b><font color=\"#FFFFFF\">[</font></b><b><font color=\"#00e5e5\">Update</font></b><b><font color=\"#FFFFFF\">]</font></b><b><font color=\"#FFFFFF\"> Release!</font></b>");
+
+
+            Game.OnUpdate += Game_OnUpdate;
+            MenuConfig.LoadMenu();
+            Spells.Initialise();
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (MenuConfig._orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo) Combo();
+            if (MenuConfig._orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed) Harass();
+            if (MenuConfig._orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear) LaneClear(); JungleClear();
+            AutoE();
+            //Recall();
+        }
+
+        public static void Combo()
+        {
+            var target = TargetSelector.GetTarget(Spells._e.Range, TargetSelector.DamageType.Physical);
+            if (target == null) return;
+
+            if (MenuConfig.UseW)
+            {
+                if (target.IsValidTarget(Spells._w.Range) && Spells._w.CanCast(target))
+                {
+                    Spells._w.Cast(target);
+                }
+            }
+
+            
+           
+
+        }
+
+        public static void Harass()
+        {
+            var target = TargetSelector.GetTarget(Spells._e.Range, TargetSelector.DamageType.Physical);
+            if (!Orbwalking.InAutoAttackRange(target) && target.GetBuffCount("twitchdeadlyvenom") >= MenuConfig.ESlider && Player.ManaPercent > 50 && Spells._e.IsReady())
+            {
+                Spells._e.Cast(target);
+            }
+            if (MenuConfig.harassW)
+            {
+                if (target.IsValidTarget(Spells._w.Range) && Spells._w.CanCast(target))
+                {
+                    Spells._w.Cast(target);
+                }
+            }
+        }
+        public static void LaneClear()
+        {
+            if (MenuConfig.laneW)
+             {
+                var minions = MinionManager.GetMinions(Player.Position, Spells._w.Range);
+                if (minions.Count >= 5)
+                    Spells._w.Cast(minions[5].ServerPosition);
+             }
+            /* Laneclear E Lasthit, Add stacks for enemies + minion lasthit
+            var minion = MinionManager.GetMinions(Player.Position, Spells._e.Range);
+            foreach (var m in minion)
+            {
+                if (m.HasBuff("twitchdeadlyvenom") && Spells._e.IsKillable(m))
+                        Spells._e.Cast(m);
+            }
+            */
+        }
+
+        public static void JungleClear()
+        {
+            if (MenuConfig._orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear)
+            {
+                var mobs = MinionManager.GetMinions(Player.Position, Spells._w.Range, MinionTypes.All,
+                       MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
+
+                if (mobs.Count != 0)
+                {
+                    Spells._w.Cast(mobs[0].ServerPosition);
+                    foreach (var m in mobs)
+                    {
+                        if (m.HasBuff("twitchdeadlyvenom"))
+                        {
+                           if(Spells._e.IsKillable(m)) 
+                            Spells._e.Cast();
+                        }
+                    }
+                }
+            }
+        }
+
+        /* Recall Bug with MenuConfig
+        private static void Recall()
+        {
+            if (MenuConfig.QRecall)
+            {
+                if (Spells._q.IsReady() && Spells._recall.IsReady())
+                {
+                    Spells._q.Cast();
+                    Spells._recall.Cast();
+                }
+            }
+            
+        }
+        */
+        public static void AutoE()
+        {
+            var mob = MinionManager.GetMinions(Spells._e.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
+
+            foreach (var m in mob)
+            {
+                if ((m.CharData.BaseSkinName.Contains("Dragon") || m.CharData.BaseSkinName.Contains("Baron")))
+                    if (Spells._e.IsKillable(m))
+                        Spells._e.Cast();
+
+               
+
+            }
+            if (MenuConfig.KsE)
+            {
+                foreach (
+                    var enemy in
+                        ObjectManager.Get<Obj_AI_Hero>()
+                            .Where(enemy => enemy.IsValidTarget(Spells._e.Range) && Spells._e.IsKillable(enemy)))
+                    Spells._e.Cast(enemy);
+
+            }
+        }
+        }
+    }
---
+++ Nechrito Twitch/Spells.cs
@@ -0,0 +1,21 @@
+﻿using LeagueSharp.Common;
+using LeagueSharp;
+
+namespace Nechrito_Twitch
+{
+    class Spells
+    {
+        private static Obj_AI_Hero Player = ObjectManager.Player;
+        public static SpellSlot Ignite, Flash;
+        public static Spell _q, _q2, _q3, _w, _e, _r, _recall;
+
+        public static void Initialise()
+        {
+            _w = new Spell(SpellSlot.W, 950);
+           _w.SetSkillshot(0.25f, 120f, 1400f, false, SkillshotType.SkillshotCircle);
+            _e = new Spell(SpellSlot.E, 1200);
+            _r = new Spell(SpellSlot.R, 900);
+            _recall = new Spell(SpellSlot.Recall);
+        }
+    }
+}
