0d1f2755770737c7897a7e48c2a113cf0e89b947
Nechrito <nechrito@live.se>
2016-04-02 03:29:42 +0200

    Simple Stuff

---
+++ Mastery Emote/Mastery Emote/MenuConfig.cs
@@ -0,0 +1,28 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Mastery_Emote
+{
+    class MenuConfig
+    {
+        public static Menu Config;
+        public static string menuName = "Mastery Emote";
+        public static void LoadMenu()
+        {
+            Config = new Menu(menuName, menuName, true);
+            var E = new Menu("Emote", "Emote");
+            E.AddItem(new MenuItem("MasteryEmote", "Mastery").SetValue(true));
+            E.AddItem(new MenuItem("Laugh", "Laugh").SetValue(true));
+            E.AddItem(new MenuItem("Taunt", "Taunt").SetValue(false));
+            E.AddItem(new MenuItem("Talk", "Joke").SetValue(false));
+            E.AddItem(new MenuItem("Dance", "Dance").SetValue(false));
+            Config.AddSubMenu(E);
+            Config.AddToMainMenu();
+        }
+        public static bool Laugh => Config.Item("Laugh").GetValue<bool>();
+        public static bool Taunt => Config.Item("Taunt").GetValue<bool>();
+        public static bool Talk => Config.Item("Talk").GetValue<bool>();
+        public static bool Dance => Config.Item("Dance").GetValue<bool>();
+        public static bool MasteryEmote => Config.Item("MasteryEmote").GetValue<bool>();
+    }
+}
---
+++ Mastery Emote/Mastery Emote/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+
+namespace Mastery_Emote
+{
+    class Program
+    {
+        public static Menu Menu;
+        private static void Main() => CustomEvents.Game.OnGameLoad += OnGameLoad;
+        private static void OnGameLoad(EventArgs args)
+        {
+            Game.PrintChat("<b><font color=\"#FFFFFF\">[</font></b><b><font color=\"#00e5e5\">Mastery Emote</font></b><b><font color=\"#FFFFFF\">]</font></b>");
+            Game.OnNotify += OnNotify;
+            MenuConfig.LoadMenu();
+        }
+        private static void OnNotify(GameNotifyEventArgs args)
+        {
+            if (args.EventId == GameEventId.OnChampionKill && MenuConfig.MasteryEmote)
+            {
+                Game.Say("/masterybadge");
+            }
+            if (args.EventId == GameEventId.OnChampionKill && MenuConfig.Laugh)
+            {
+                Game.Say("/l");
+            }
+            if (args.EventId == GameEventId.OnChampionKill && MenuConfig.Talk)
+            {
+                Game.Say("/j");
+            }
+            if (args.EventId == GameEventId.OnChampionKill && MenuConfig.Taunt)
+            {
+                Game.Say("/t");
+            }
+            if (args.EventId == GameEventId.OnChampionKill && MenuConfig.Dance)
+            {
+                Game.Say("/d");
+            }
+        }
+    }
+}
